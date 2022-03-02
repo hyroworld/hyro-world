2d9e570881a68b17fcd25aa0bb97579e5a584e60
장혜승 <hyesech@gmail.com>
2022-03-02 10:36:42 +0900

    init code

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Hyro {name}!</h1>
+	<p>Everything begins with your stance.</p>
+    <p>Remember.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+	h1 {
+		color: #0000ff;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
